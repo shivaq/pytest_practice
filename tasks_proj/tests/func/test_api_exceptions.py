ed0ddf94fb21b1e4fd50308d7aeef31deff43d8c
Yasuaki Shibata <shivaq777@gmail.com>
2019-12-16 02:44:54 +0900

    test

--- tasks_proj/tests/func/test_api_exceptions.py
+++ tasks_proj/tests/func/test_api_exceptions.py
@@ -2,6 +2,22 @@ import pytest
 import tasks
 
 
-def test_add_raises():
+def test_raises_typeerror_in_add_with_not_a_task_object():
     with pytest.raises(TypeError):
         tasks.add(task='not a Task object')
+
+
+def test_start_tasks_db_raises():
+    with pytest.raises(ValueError) as excinfo:
+        tasks.start_tasks_db('some/great/path', 'mysql')
+
+        exception_msg = excinfo.value.args[0]
+
+        assert exception_msg == "db_type must be a 'tiny' or 'mongo'"
+
+
+# put custom decorator for pytest
+@pytest.mark.smoke
+def test_list_raises():
+    with pytest.raises(TypeError):
+        tasks.list_tasks(owner=123)
